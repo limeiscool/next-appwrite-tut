fa9c219b6350f8f0cc426640fe6665f4b50dbe95
Richard Marshall <rich.qazwsx@gmail.com>
2024-04-12 01:56:09 -0700

    Init/pages

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="flex-col items-center justify-center">
+      <h1 className="text-center text-white text-2xl">Login</h1>
+    </div>
+  )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function SignupPage() {
+  return (
+    <div className="flex-col items-center justify-center">
+      <h1 className="text-center text-white text-2xl">Signup</h1>
+    </div>
+  )
+}
