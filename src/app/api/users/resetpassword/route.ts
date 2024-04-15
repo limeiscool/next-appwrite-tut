9d455a9ed44f82f52afc944f8ee9920dfe872f50
Richard Marshall <rich.qazwsx@gmail.com>
2024-04-14 22:06:00 -0700

    feat: users can now reset passwords

---
+++ src/app/api/users/resetpassword/route.ts
@@ -0,0 +1,42 @@
+import { connect } from "@/db/config";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import bcrypt from "bcryptjs";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { token, password} = reqBody;
+    const user = await User.findOne({forgotPasswordToken: token, 
+      forgotPasswordTokenExpires: {
+        $gt: Date.now()
+      }
+    })
+    if (!user) {
+      return NextResponse.json({
+        error: "Invalid token"
+      }, {status: 400})
+    }
+    if (bcrypt.compareSync(password, user.password)) {
+      return NextResponse.json({
+        error: "New password cannot be the same as old password"
+      }, {status: 400})
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    
+    user.password = hashedPassword;
+    user.forgotPasswordToken = undefined;
+    user.forgotPasswordTokenExpires = undefined;
+    await user.save();
+    return NextResponse.json({
+      message: "Password reset successfully"
+    })
+  } catch (error:any) {
+    return NextResponse.json({
+      error: error.message
+    }, {status: 500})
+  }
+}
--- src/app/resetpassword/page.tsx
+++ src/app/resetpassword/page.tsx
@@ -4,10 +4,38 @@ import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function ResetPasswordPage() {
   const router = useRouter();
   const [token, setToken] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [matching, setMatching] = useState(false);
+  const [bothPasswords, setBothPasswords] = useState(false);
+  const [password, setPassword] = useState("");
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  const inputClassSnip = bothPasswords ? matching ? "border-green-500" : "border-rose-700" : "border-gray-300"
+
+  const sendResetRequest = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.post('/api/users/resetpassword', {token, password});
+      console.log(res.data);
+      toast.success("📧 Password was reset successfully")
+      router.push('/login');
+    } catch (error:any) {
+      console.log(error.response.status + " " + error.response.data.error)
+      toast.error(error.response.status + " " + error.response.data.error)
+      router.push('/login');
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  
 
   useEffect(() => {
     const urlToken = window.location.search.split('=')[1];
@@ -18,12 +46,56 @@ export default function ResetPasswordPage() {
     }
   }, [router]);
 
-  // NEED TO MAKE FORM !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  useEffect(() => {
+    if (newPassword && confirmPassword) {
+      setBothPasswords(true);
+      if (newPassword === confirmPassword) {
+        setButtonDisabled(false);
+        setPassword(newPassword);
+        setMatching(true);
+      } else {
+        setButtonDisabled(true);
+        setMatching(false);
+      }
+    } else {
+      setBothPasswords(false);
+      setButtonDisabled(true);
+      setMatching(false);
+    }
+  }, [newPassword, confirmPassword]);
+
+  
+
+  
+
 
   return (<>{
     token &&
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl">Reset Password</h1>
+      <h1 className="text-4xl mb-4">Reset Password</h1>
+      <hr />
+
+      <input
+        type="password"
+        placeholder="New Password"
+        className={inputClassSnip + " text-black p-2 border-4 rounded-lg mb-4 focus:outline-none"}
+        onChange={(e) => setNewPassword(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Confirm New Password"
+        className={inputClassSnip + " text-black p-2 border-4 rounded-lg mb-4 focus:outline-none"}
+        onChange={(e) => setConfirmPassword(e.target.value)}
+      />
+      <button
+        className={inputClassSnip + " p-2 border rounded-lg mb-4 focus:ouline-none"}
+        onClick={() => sendResetRequest()}
+        disabled={buttonDisabled}
+      >
+        Reset Password
+      </button>
+      <Toaster />
+
     </div>
   }</>)
 }
